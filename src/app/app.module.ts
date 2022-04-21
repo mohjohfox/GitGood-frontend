b4721a483e5a304d1048d20c761d8e80f8d93bc8
mohjohfox <marcorobinf@gmail.com>
2022-04-21 19:20:32 +0200

    GG-2: make gamemode buttons independent from other buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ButtonComponent } from './components/atoms/button/button.component';
-import { ItemViewComponent } from './components/pages/item-view/item-view.component';
+import { ItemViewComponent } from './components/atoms/button/item-view/item-view.component';
 import { ItemEditComponent } from './components/pages/item-edit/item-edit.component';
 import { ListViewComponent } from './components/pages/list-view/list-view.component';
 import {GameSetupComponent} from './components/pages/game-setup/game-setup.component';
