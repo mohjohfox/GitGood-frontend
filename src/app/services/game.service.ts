e379c8ff93dab1c3e271e166ec29e4e572a2e9ab
mohjohfox <marcorobinf@gmail.com>
2022-05-20 08:09:40 +0200

    added action for feature branches - splitted steps into build, test and deployment - configure baseUrl for backend in enviroment.ts

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -2,30 +2,30 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs';
 import {Game} from '../model/game';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
 
-  private url = 'https://backend.gitgood.mohjohfox.de';
-  // private url = 'http://localhost:8080';
+  private apiUrl = environment.baseURL;
 
   constructor(private readonly httpClient: HttpClient) {
   }
 
   getGameFromServer(gameId: string): Observable<Game> {
-    return this.httpClient.get<Game>(this.url + '/game/' + gameId);
+    return this.httpClient.get<Game>(this.apiUrl + '/game/' + gameId);
   }
 
   sendSubmitRound(gameId: string, thrownPoints: string[]): Observable<Game> {
     const headers = { 'content-type': 'application/json'};
     const body = JSON.stringify(thrownPoints);
-    return this.httpClient.post<Game>(this.url +  '/game/' + gameId + '/round/submit', body, {headers});
+    return this.httpClient.post<Game>(this.apiUrl +  '/game/' + gameId + '/round/submit', body, {headers});
   }
 
   deleteGameById(gameId: string) {
-    return this.httpClient.delete(this.url + '/delete/' + gameId).subscribe();
+    return this.httpClient.delete(this.apiUrl + '/delete/' + gameId).subscribe();
   }
 
 }
