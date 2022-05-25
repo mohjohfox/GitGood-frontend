4f1f34ac1248f3ff794d8df418454be5c311119e
Alisa Fien <alisa1fien@gmail.com>
2022-05-25 20:48:36 +0200

    Added leaderboard component to winner page

--- src/app/components/pages/winner/winner.component.ts
+++ src/app/components/pages/winner/winner.component.ts
@@ -1,7 +1,8 @@
-import {Component, Input, OnDestroy, OnInit, Output} from '@angular/core';
+import {Component, Input, OnDestroy, ViewChild} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {GameService} from '../../../services/game.service';
-import {isEmpty} from 'rxjs';
+import {LeaderboardComponent} from '../../organisms/leaderboard/leaderboard.component';
+import {Game} from '../../../model/game';
 
 @Component({
   templateUrl: './winner.component.html',
@@ -11,14 +12,22 @@ export class WinnerComponent implements OnDestroy {
   @Input()
   winner: string;
   gameId: string;
+  @ViewChild(LeaderboardComponent) leaderboard!: LeaderboardComponent;
 
   constructor(private readonly router: Router, activatedRoute: ActivatedRoute, private gameService: GameService) {
     this.gameId = activatedRoute.snapshot.paramMap.get('gameId');
-    this.gameService.getGameFromServer(this.gameId).subscribe(value => {
-      this.winner = value.currentPlayer.name;
+    this.gameService.getGameFromServer(this.gameId).subscribe(game => {
+      this.sortLeaderboardAccordingToGameMode(game);
     });
   }
 
+
+  private sortLeaderboardAccordingToGameMode(value: Game) {
+    if (value.gameMode.name === '301' || value.gameMode.name === '501' ) {
+      this.leaderboard.playersAsArray = value.players.sort((p1, p2) => p1.points - p2.points);
+      this.winner = this.leaderboard.playersAsArray[0].name;
+    }
+  }
 
   ngOnDestroy() {
     this.gameService.deleteGameById(this.gameId);
